482b8a10361c66e97b6f9f72db21b9f7cb143b37
Feng Xiong <fengxiong34@gmail.com>
2020-06-17 14:52:31 -0400

    modified app.js structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import store from './store/index.js';
 import Header from './components/Header.js';
 import Selector from './components/Selector.js';
+import store from './store/index.js';
 import { Provider } from 'react-redux';
 import { GlobalStyle } from './style';
 import { GlobalStyleFont } from './static/iconfont/iconfont';
 
 function App() {
   return (
-    <div className='App'>
-      <Provider store={store}>
+    <Provider store={store}>
+      <div>
         <Header />
         <Selector />
         <GlobalStyle />
         <GlobalStyleFont />
-      </Provider>
-    </div>
+      </div>
+    </Provider>
   );
 }
 
